0e229f326d1ccfb1b0c5440f721d1432ce99bffa
godinezseba <sebastian.godinez@hotmail.com>
2017-11-16 19:45:18 -0300

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -1,6 +1,6 @@
-
-public class Jugador extends Personaje{
-	public Jugador(int vida_inicial){
-		super(vida_inicial);
+public class Jugador extends Personaje_Ataca{
+	String Nombre;
+	public Jugador(int vida_inicial,int ataque_inicial, int defensa_inicial){
+		super(vida_inicial,ataque_inicial,defensa_inicial);
 	}
 }
--- Personaje.java
+++ Personaje.java
@@ -1,4 +1,5 @@
 abstract class Personaje{
+	
   protected int vida_actual;
   protected int vida_base;
   
@@ -8,7 +9,13 @@ abstract class Personaje{
     vida_base=vida_inicial;
   }
   
-  /** Entrega la vida actual de un NPC, si este tiene defenesa base se la suma */
-  abstract int vida_actual(); 
+  /** Entrega la vida actual de un NPC, si este tiene defenesa base, objetos, etc.
+   *  se la suma. */
+  abstract int vida_actual_enAtaque();
+  /** Esta es para mostrar en Pantalla, solo considera la vida_actual. */
+  abstract int vida_actual_IO();
+  /** Afecta la vida del NPC en cuestion, retorna la vida resultante, la implementacion
+   * dependera si nuestro NPC tiene defensa, objetos, etc.*/
+  abstract int cambio_vida();
 }
 
