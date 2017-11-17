3e1b25eaf04f51c8ef7b2b61cf90234191e3f346
soniso17 <32210150+soniso17@users.noreply.github.com>
2017-11-17 14:47:29 -0300

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -1,11 +1,12 @@
-public class Enemigo extends Personaje_Ataca {
+public class Enemigo extends Personaje_Ataca implements Ataque {
 	
 	public Enemigo (int vida_inicial, int ataque_inicial, int defensa_inicial ) {
 		super(vida_inicial, ataque_inicial, defensa_inicial,"malo");
 		 
 	}
+	
 	public int vida_actual_enAtaque() {
-		return 0;
+		return vida_actual+defensa_base;
 	}
 	public int vida_actual_IO() {
 		return vida_actual;
@@ -13,8 +14,16 @@ public class Enemigo extends Personaje_Ataca {
 	public int cambio_vida(int ataque) {
 		int cambio_vida;
 		cambio_vida = vida_actual_enAtaque() - ataque;
+		if(cambio_vida<0) {
+			cambio_vida=0;
+		}
+		vida_actual=cambio_vida;
 		return cambio_vida;
 	}
+	public int Atacar() {
+		return ataque_base;
+	}
+	
 	
 }
 	
