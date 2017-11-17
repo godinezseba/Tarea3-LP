7a7c4b07098b16f6de10cc812881b2b09cb2bb3e
godinezseba <sebastian.godinez@hotmail.com>
2017-11-17 13:55:13 -0300

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -1,8 +1,11 @@
 public class Jugador extends Personaje_Ataca{
 	String Nombre;
 	Objeto cant_objt[];
-	public Jugador(int vida_inicial,int ataque_inicial, int defensa_inicial){
+	int ataque_especial1[]; // Ataque Especial 1, [pp, ataque] 
+	int ataque_especial2[]; // Ataque Especial 2, [pp, ataque]
+	public Jugador(int vida_inicial,int ataque_inicial, int defensa_inicial,String Name){
 		super(vida_inicial,ataque_inicial,defensa_inicial,"bueno");
+		Nombre = Name;
 	}
 	public int vida_actual_enAtaque() {
 		return 0;
