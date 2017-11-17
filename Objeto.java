0d2a46f00e911ecf0d49a5891d4ad1c71d1477a7
godinezseba <sebastian.godinez@hotmail.com>
2017-11-16 21:33:54 -0300

    Add files via upload

---
+++ Objeto.java
@@ -0,0 +1,20 @@
+public class Objeto {
+	int bonus;
+	String tipo; // se especifica si es a la vida, ataque o defensa "vida", "ataque", "defensa"
+	
+	public Objeto(int valor, String ntipo) {
+		bonus = valor;
+		tipo = ntipo;
+	}
+	/** Retorna de que tipo es el bonus en cuestion.*/
+	public String tipo() {
+		return tipo;
+	}
+	/**Retorna el valor del bonus.*/
+	public int Bonus() {
+		return bonus;
+	}
+	public String ToString() {
+		return tipo+bonus;
+	}
+}
