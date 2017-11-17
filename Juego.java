39004684687e53f34a10da08f295b5decb460573
godinezseba <sebastian.godinez@hotmail.com>
2017-11-17 19:36:32 -0300

    Add files via upload

--- Juego.java
+++ Juego.java
@@ -5,9 +5,12 @@ public class Juego{
 	Nivel cuarta_dimension;
 	static int  opcion=-1;
 	static Scanner terminal;
+	static String temp = "";
 	
 	public static void crear_jugador() {
 		String nombreaux;
+		int auxvida,aux;
+		System.out.println("Nombre del Jugador: ");
 		
 	}
 	public static void main (String [ ] args) {
@@ -21,12 +24,14 @@ public class Juego{
 			System.out.println("3. Consultar información de Nivel");
 			System.out.println("4. Salir");
 			
-			opcion=terminal.nextInt();
+			opcion = terminal.nextInt();
 			while(opcion > 4 || opcion < 1) {
 				System.out.println("Ingrese otra opcion: ");
-				opcion=terminal.nextInt();
+				opcion = terminal.nextInt();
 			}
 			if(opcion == 1) {
+				temp = terminal.nextLine();
+				System.out.println(temp);
 				crear_jugador();
 			}
 
