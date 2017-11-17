c46691ede38a00ef4aab1f79f3533308331a461d
soniso17 <32210150+soniso17@users.noreply.github.com>
2017-11-17 14:17:28 -0300

    Update Ataque.java

--- Ataque.java
+++ Ataque.java
@@ -1,5 +1,7 @@
 public interface Ataque{
 	/** Esta funcion se aplica al NPC que va a atacar y retorna el numero
-	 * de daño, incluyendo todos los objetos que se aplicaran.*/
+	 * de daÃ±o, incluyendo todos los objetos que se aplicaran.*/
 	int Atacar();
+	int get_ataque();
+	int get_defensa();
 }
