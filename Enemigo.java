bc841b5f0c7707393e8ac96fdb6c10bf4f221f24
soniso17 <32210150+soniso17@users.noreply.github.com>
2017-11-17 14:04:54 -0300

    Add files via upload

--- Enemigo.java
+++ Enemigo.java
@@ -4,11 +4,17 @@ public class Enemigo extends Personaje_Ataca {
 		super(vida_inicial, ataque_inicial, defensa_inicial,"malo");
 		 
 	}
-	
-	
-	
-	
+	public int vida_actual_enAtaque() {
+		return 0;
+	}
+	public int vida_actual_IO() {
+		return vida_actual;
+	}
+	public int cambio_vida(int ataque) {
+		int cambio_vida;
+		cambio_vida = vida_actual_enAtaque() - ataque;
+		return cambio_vida;
+	}
 	
 }
 	
-}
