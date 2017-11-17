f8a5893758a70ef995f8536156ff90f6156eeb21
soniso17 <32210150+soniso17@users.noreply.github.com>
2017-11-17 14:51:54 -0300

    Update Aliado.java

--- Aliado.java
+++ Aliado.java
@@ -10,7 +10,7 @@ public class Aliado extends Personaje {
 		return objeto1;
 	}
 	public int vida_actual_enAtaque() {
-		return 0;
+		return vida_actual;
 	}
 	public int vida_actual_IO() {
 		return vida_actual;
@@ -18,8 +18,11 @@ public class Aliado extends Personaje {
 	public int cambio_vida(int ataque) {
 		int cambio_vida;
 		cambio_vida = vida_actual_enAtaque() - ataque;
+		if(cambio_vida<0) {
+			cambio_vida=0;
+		}
+		vida_actual=cambio_vida;
 		return cambio_vida;
 	}
-	
-	
+		
 }
