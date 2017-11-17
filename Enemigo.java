d2ebd33363dec8b4167a8a71ff0081a25bb66cdf
soniso17 <32210150+soniso17@users.noreply.github.com>
2017-11-17 14:00:32 -0300

    Create Enemigo.java

---
+++ Enemigo.java
@@ -0,0 +1,14 @@
+public class Enemigo extends Personaje_Ataca {
+	
+	public Enemigo (int vida_inicial, int ataque_inicial, int defensa_inicial ) {
+		super(vida_inicial, ataque_inicial, defensa_inicial,"malo");
+		 
+	}
+	
+	
+	
+	
+	
+}
+	
+}
