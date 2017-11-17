710694e731e5bbc1096bd4072dfecf3e5756a516
godinezseba <sebastian.godinez@hotmail.com>
2017-11-17 13:58:26 -0300

    Delete personaje_ataca.java

--- personaje_ataca.java
+++
@@ -1,9 +0,0 @@
-public class personaje_ataca extends Personaje{
-  protected int ataque;
- 
-  public personaje_ataca(int vida_inicial, int ataque_inicial){
-    super(vida_inicial);
-    ataque = ataque_inicial;
-  }
-}
-
